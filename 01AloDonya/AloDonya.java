5ee6dcdc4c3c8ae1c51199a9455dd8242198da11
Muhammad Zulfikar <v_karkar@windowslive.com>
2015-12-04 11:34:42 +0700

    test baru

--- 01AloDonya/AloDonya.java
+++ 01AloDonya/AloDonya.java
@@ -11,6 +11,6 @@ public class AloDonya
     // instance variables - replace the example below with your own
     public static void main (String[]args){
     
-    System.out.println("Alo Donya");
+    System.out.println("Alo Donya mandum");
     }
 }
